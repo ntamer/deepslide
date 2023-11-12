1e0681b9e22c69faa378f188018c66381743aa0f
ntamer <tamer3@comcast.net>
2023-11-11 19:49:29 -0800

    Update and rename nt_get_inferences to nt_get_inferences.py

--- code/nt_get_inferences.py
+++ code/nt_get_inferences.py
@@ -4,7 +4,16 @@ from utils_evaluation import (parse_thresholds,output_all_predictions)
 # Running the code on the testing set.
 print("\n\n+++++ Running nt_get_inferences.py +++++")
 print("\n----- Reading best thresholds -----")
-best_thresholds = parse_thresholds(thresholds_folder=config.args.inference_test)
+thresholds_folder=config.args.inference_test
+ls_output = os.popen('ls {thresholds_folder}/*').read()
+# Split the output into a list of strings
+files = ls_output.splitlines()
+# Get the first file in the list (there should only be one file)
+first_file = files[0]
+print('Thresholds file: ' + first_file)
+best_thresholds = parse_thresholds(Path(first_file))
+print(best_thresholds)
+os.rename(first_file, first_file + '_training')
 print("----- Finished reading best thresholds -----\n")
 print("----- Outputting all predictions -----")
 output_all_predictions(patches_pred_folder=config.args.preds_test,
